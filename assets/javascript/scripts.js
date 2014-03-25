9586654ee9dc5002072f8334aa69145696bd743e
Dustin Armstrong <dustin@dustinarmstrong.com>
2014-03-25 11:25:26 -0400

    Site updated at 2014-03-25 15:25:26 UTC

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,4 +1,13 @@
-$('.highlight').click( function() {
-	$(this).addClass('expand');
-	alert("hello");
-});
+$(document).ready(
+	$('.highlight').click( function() {
+		if($(this).hasClass('expand')) {
+			$(this).removeClass('expand');
+		}
+		else {
+			$(this).addClass('expand');
+		}
+	}),
+	$('.menu-toggle').click( function() {
+		$('#header').toggleClass('active');
+	})
+);
